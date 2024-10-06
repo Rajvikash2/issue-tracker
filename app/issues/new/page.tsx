9f438408415a9791ae2f8bcea77db24e29e900a1
Rajvikash <rajvikash.r2022cse@sece.ac.in>
2024-10-06 14:48:16 +0530

    simpleMDE added

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,14 +1,15 @@
+'use client';
 
-
-import { TextArea, TextField, Button } from '@radix-ui/themes'
-import React from 'react'
+import { TextField, Button } from '@radix-ui/themes'
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css"
 
 const newIssue = () => {
   return (
     <div className='max-w-xl space-y-2'>
         <TextField.Root placeholder='Title'>
         </TextField.Root>
-        <TextArea placeholder="Description" />
+        <SimpleMDE placeholder="Description" />
         <Button> Submit Issue </Button>
     </div>
   )
