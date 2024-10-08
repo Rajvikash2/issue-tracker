f50becd9bca615d7a5af880ed81ce8433af57311
Rajvikash <rajvikash.r2022cse@sece.ac.in>
2024-10-08 19:22:34 +0530

    API for updating issue created

---
+++ app/api/issues/[id]/route.ts
@@ -0,0 +1,27 @@
+import { createIssueSchema } from "@/app/validationSchema";
+import prisma from "@/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+
+export async function PATCH (request:NextRequest,{params} :{params:{id:string}}){
+    const body = await request.json();
+    const valid = createIssueSchema.safeParse(body);
+    if(!valid.success){
+      return  NextResponse.json(valid.error.format(),{status:400})
+    }
+    const issue =  await prisma.issue.findUnique({
+        where:{id:parseInt(params.id)}
+    })
+    if(!issue){
+       return NextResponse.json("Invalid issue",{status:404});
+    }
+
+  const updatedIssue = await  prisma.issue.update({
+        where : {id:issue.id},
+        data:{
+            title:body.title,
+            description:body.description
+        }
+    })
+    return NextResponse.json(updatedIssue);
+}
