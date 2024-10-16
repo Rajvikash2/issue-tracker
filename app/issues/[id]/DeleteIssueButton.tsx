77fe56ad0135cd3eccb5180eac50f30032242c0a
Rajvikash <rajvikash.r2022cse@sece.ac.in>
2024-10-16 19:24:31 +0530

    added spinner while deleting

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,4 +1,5 @@
 'use client'
+import Spinner from '@/app/components/Spinner'
 import { AlertDialog, Button, Flex } from '@radix-ui/themes'
 import axios from 'axios'
 import { useRouter } from 'next/navigation'
@@ -7,13 +8,15 @@ import React, { useState } from 'react'
 const DeleteIssueButton = ({issueId}:{issueId:number}) => {
     const router = useRouter();
     const [error,setError] = useState(false);
-
+    const [isDeleting, setDelete] = useState(false);
     const deleteIssue = async()=>{
-        try {            
+        try {   
+            setDelete(true);         
             await axios.delete('/api/issues/'+issueId)
             router.push('/issues');
             router.refresh();                        
         } catch (error) {
+            setDelete(false);
             setError(true);
         }
     }
@@ -21,7 +24,10 @@ const DeleteIssueButton = ({issueId}:{issueId:number}) => {
     <div>
     <AlertDialog.Root>
         <AlertDialog.Trigger>
-            <Button color='red'>Delete Issue</Button>
+            <Button color='red' disabled={isDeleting}>
+                Delete Issue
+            {isDeleting && <Spinner/>}
+            </Button>
         </AlertDialog.Trigger>
         <AlertDialog.Content>
             <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
