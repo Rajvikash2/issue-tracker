c09b8500672439e20c855f69c081d93f302775b8
Rajvikash <rajvikash.r2022cse@sece.ac.in>
2024-10-19 00:19:05 +0530

    added dropdown menu for user login

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -4,48 +4,70 @@ import React from 'react'
 import { usePathname } from 'next/navigation';
 import { GiAutoRepair } from "react-icons/gi";
 import classNames from 'classnames';
-import { useSession } from 'next-auth/react';
-import { Box, Container, Flex } from '@radix-ui/themes';
-const Navbar = () => {
-    const {status, data : session} = useSession();
-    const pathname = usePathname();
-    const links =[
-        {label:'Dashboard', href:'/'},
-        {label:'Issues', href:'/issues'},
-    ]
-  return (
-   
-    <nav className=' space-x-6 border-b mb-5 px-5 py-4'>
-        <Container>
-        <Flex justify='between'>
-            <Flex gap='3' align='center'>
-            <Link href="/"><GiAutoRepair /></Link>
-        <ul className='flex space-x-5'>
-            {links.map(link=>
-            <li key={link.href}>
-            <Link 
-            className={ classNames({
-                'text-zinc-900' : link.href===pathname,
-                'text-zinc-500': link.href!==pathname,
-                'hover:text-zinc-800 transition-colors' : true
-            })}
-            href={link.href}>{link.label}
-            </Link>
-            </li>)}  
-        </ul>
-            </Flex>
-            <Box>
-            {status === 'authenticated' ? (
-                <Link href='/api/auth/signout'>Log Out</Link>
-            ) : (
-                <Link href='/api/auth/signin'>Login</Link>
-            )}
-          </Box>
-        </Flex>
-        </Container>
-          
-    </nav>
-  )
-}
+import { useSession } from 'next-auth/react'
+import { Avatar, Box, Container, DropdownMenu, Flex, Text } from '@radix-ui/themes';
 
-export default Navbar
+const Navbar = () => {
+    const pathname = usePathname();
+    const { status, data: session } = useSession();
+    
+    const links = [
+        { label: 'Dashboard', href: '/' },
+        { label: 'Issues', href: '/issues' },
+    ];
+
+    return (
+        <nav className='border-b mb-5 px-5 py-4'>
+            <Container>
+                <Flex justify='between'>
+                    <Flex align='center' gap='3'>
+                        <Link href="/"><GiAutoRepair /></Link>
+                        <ul className='flex space-x-5'>
+                            {links.map(link => (
+                                <li key={link.href}>
+                                    <Link 
+                                        className={classNames({
+                                            'text-zinc-900': link.href === pathname,
+                                            'text-zinc-500': link.href !== pathname,
+                                            'hover:text-zinc-800 transition-colors': true
+                                        })}
+                                        href={link.href}
+                                    >
+                                        {link.label}
+                                    </Link>
+                                </li>
+                            ))}        
+                        </ul>
+                    </Flex>
+
+                    <Box>
+                        {status === 'authenticated' && (
+                            <DropdownMenu.Root>
+                                <DropdownMenu.Trigger>
+                                    <Avatar src={session.user!.image!} fallback='P' size='3' radius='full' className='cursor-pointer' />
+                                </DropdownMenu.Trigger>
+                                <DropdownMenu.Content>
+                                    <DropdownMenu.Label>
+                                        <Text size='3'>
+                                        {session.user!.email}
+                                        </Text>
+                                    </DropdownMenu.Label>
+                                    <DropdownMenu.Separator />
+                                    <DropdownMenu.Item>
+                                        <Link href='/api/auth/signout'>Sign Out</Link>
+                                    </DropdownMenu.Item>
+                                </DropdownMenu.Content>
+                            </DropdownMenu.Root>
+                        )}
+
+                        {status === 'unauthenticated' && (
+                            <Link href='/api/auth/signin'>Log In</Link>
+                        )}
+                    </Box>
+                </Flex>       
+            </Container>
+        </nav>
+    );
+};
+
+export default Navbar;
