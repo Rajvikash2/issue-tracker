be3c3bb81a68efd875aa9ae100d81b4f021caace
Rajvikash <rajvikash.r2022cse@sece.ac.in>
2024-10-18 22:18:49 +0530

    added a comment next auth api

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
 
-// auth
+// auth providers added need to add adapter
 const handler = NextAuth({
   providers:[
     GoogleProvider({
