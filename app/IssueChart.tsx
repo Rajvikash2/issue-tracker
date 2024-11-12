18f708b448153fdf6ec28384996e569d84e1067b
Rajvikash <rajvikash.r2022cse@sece.ac.in>
2024-11-12 23:42:32 +0530

    Layout the dashboard

--- app/IssueChart.tsx
+++ app/IssueChart.tsx
@@ -1,32 +1,34 @@
 'use client';
-import { Card } from '@radix-ui/themes'
-import { ResponsiveContainer,BarChart,XAxis,YAxis,Bar } from 'recharts'
-import React from 'react'
+import { Card } from '@radix-ui/themes';
+import { BarChart, Bar, XAxis, YAxis, ResponsiveContainer, Tooltip, Legend } from 'recharts';
+import React from 'react';
 
-
-interface Props{
-    open:number,
-    closed:number,
-    inProgress:number
+interface Props {
+    open: number;
+    closed: number;
+    inProgress: number;
 }
 
-const IssueChart = ({open,closed,inProgress}:Props) => {
+const IssueChart = ({ open, closed, inProgress }: Props) => {
     const data = [
-        {label:'Open', value:open},
-        {label:'In-Progress', value:inProgress},
-        {label:'Closed', value:closed},
-    ]
-  return (
-    <Card>
-        <ResponsiveContainer width='100%' height={300}>
-            <BarChart data={data}>
-                <XAxis dataKey="label"/>
-                <YAxis/>
-                <Bar dataKey='value' barSize={60} fill='#FFDF00'/>
-            </BarChart>
-        </ResponsiveContainer>
-    </Card>
-  )
-}
+        { label: 'Open', value: open },
+        { label: 'In-Progress', value: inProgress },
+        { label: 'Closed', value: closed },
+    ];
 
-export default IssueChart
+    return (
+        <Card>
+            <ResponsiveContainer width='100%' height={300}>
+                <BarChart data={data}>
+                    <XAxis dataKey="label" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Bar dataKey='value' barSize={60} fill='#FFDF00' />
+                </BarChart>
+            </ResponsiveContainer>
+        </Card>
+    );
+};
+
+export default IssueChart;
--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,19 @@ import LatestIssues from './LatestIssues'
 import IssueSummary from './IssueSummary'
 import prisma from '@/prisma/client'
 import IssueChart from './IssueChart'
+import { Flex, Grid } from '@radix-ui/themes'
 
 export default async function Home() {
   const openCount = await prisma.issue.count({where:{status:'OPEN'}});
   const inProgressCount = await prisma.issue.count({where:{status:'IN_PROGRESS'}});
   const ClosedCount = await prisma.issue.count({where:{status:'CLOSED'}});
   return (
-   <div>
-    {/* <LatestIssues /> */}
+   <Grid columns={{initial:'1',md:'2'}} gap='5'>
+    <Flex direction='column' gap='5'>
+      <IssueSummary open={openCount} closed={ClosedCount} inProgress={inProgressCount} />
     <IssueChart open={openCount} closed={ClosedCount} inProgress={inProgressCount}/>
-   </div>
+    </Flex>
+    <LatestIssues />
+   </Grid>
   )
 }
